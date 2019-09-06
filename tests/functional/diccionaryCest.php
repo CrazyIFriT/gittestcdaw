387a90b3cd8df8cd8111f1e7a6dce4db3781568b
Javier <javierjesus.ramirezvelazquez_29@alumno.academiadf.com>
2019-09-06 19:49:07 +0100

    New test for frontpage

--- tests/functional/diccionaryCest.php
+++ tests/functional/diccionaryCest.php
@@ -24,4 +24,42 @@ class diccionaryCest
         $I->amOnRoute("diccionary/view", ['id' => $model->id]);
         $I->seeResponseCodeIs(200);
     }
+
+    public function indexPageTest(FunctionalTester $I){
+        $I->amOnRoute("diccionary");
+        $I->see("Diccionaries", "h1");
+        $I->click('Create Diccionary');
+        $I->see('Create Diccionary','h1');
+    }
+
+    public function createTest(FunctionalTester $I){
+        $I->amGoingTo('Test Submit Diccionary');
+        $I->amOnRoute("diccionary/create");
+        $I->fillField('#diccionary-word', 'ads');
+        $I->fillField('#diccionary-language', 'EN');
+        $I->fillField('#diccionary-translate', 'testeo');
+        $I->click('Save');
+        $model = Diccionary::findOne(['word'=>'ads']);
+        $I->see($model->id,'h1');
+        $I->seeResponseCodeIs(200);
+    }
+
+    public function updateTest(FunctionalTester $I){
+        $model = new Diccionary(['word' => 'pasado', 'language' => 'EN', 'translate' => 'past']);
+        $model->save();
+        $I->amOnRoute("diccionary/update", ['id' => $model->id]);
+        $I->fillField('#diccionary-word', 'present');
+        $I->click('Save');
+        $I->see($model->id,'h1');
+        $test = Diccionary::findOne(['word'=>'present']);
+        $I->see($test->word,'td');
+    }
+    public function updateTestFails(FunctionalTester $I){
+        $model = new Diccionary(['word' => 'pasado', 'language' => 'EN', 'translate' => 'past']);
+        $model->save();
+        $I->amOnRoute("diccionary/update", ['id' => $model->id]);
+        $I->fillField('#diccionary-word', 'aaa');
+        $I->click('Save');
+        $I->dontsee($model->word,'td');
+    }
 }
--- views/diccionary/index.php
+++ views/diccionary/index.php
@@ -15,7 +15,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create Diccionary', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Create Diccionary', ['create'], ['class' => 'btn btn-success', 'name' => 'create-button']) ?>
     </p>
 
     <?php Pjax::begin(); ?>
