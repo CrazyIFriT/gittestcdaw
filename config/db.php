4aa6182eeb89fbd0e7b0023fdd1d9e1e63c813b5
CrazyIFriT <CrazyIFriT@users.noreply.github.com>
2019-09-11 11:27:17 +0100

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=diccionary',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'proyecto',
+    'password' => 'proyecto',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
